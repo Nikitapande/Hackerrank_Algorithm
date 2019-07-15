2ab29e39d04806a23aafeed44e1d9ce2d95a892d
Nikita pande <30722238+Nikitapande@users.noreply.github.com>
2019-07-15 22:37:49 +0530

    Update problem4.cpp

--- problem4.cpp
+++ problem4.cpp
@@ -1,3 +1,10 @@
+/*	Given a square matrix of size N×N, calculate the absolute difference between the sums of its diagonals.
+	Input Format
+	    The first line contains a single integer, N. The next N lines denote the matrix's rows, with each line 
+	      containing N space-separated integers describing the columns.
+	Output Format
+	   Print the absolute difference between the two sums of the matrix's diagonals as a single integer.
+*/
 #include<iostream>
 #include <bits/stdc++.h> 
 using namespace std;
