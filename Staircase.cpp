c3dc8f0de173a641459e6d307ac8dbc24f9da656
Nikita pande <30722238+Nikitapande@users.noreply.github.com>
2019-07-16 21:39:30 +0530

    Create Staircase.cpp

---
+++ Staircase.cpp
@@ -0,0 +1,33 @@
+/*
+  Consider a staircase of size :
+
+   #
+  ##
+ ###
+####
+
+*/
+
+#include <bits/stdc++.h>
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n,i,j,k;
+    cin >> n;
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n-i;j++)
+        {
+            cout<<" ";
+        }
+        
+        for(k=1;k<=i;k++)
+        {
+            cout<<"#";
+        }
+            cout<<"\n";
+    }
+    return 0;
+}
